cf3ba5cae799333146982b0a91053d3145423350
unknown <thasalechetana@gmail.com>
2026-08-28 06:25:09 +0000

    api changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,7 +5,7 @@ export const api = axios.create({
 
     baseURL:
         import.meta.env.VITE_API_URL ||
-        (import.meta.env.DEV ? 'https://sahib-s-gurudwara.onrender.com/api' : '/api'),
+        'https://sahib-s-gurudwara.onrender.com/api',
 
     headers: {
         'Content-Type': 'application/json'
